034604370c4b1fd1c06f89e9e70418c36a2a9009
Jonathan Reifer <jonathanr@osrce.com>
2013-07-11 11:01:04 -0400

    added ejs and socket.io support.

--- node/app.js
+++ node/app.js
@@ -2,6 +2,11 @@
 
 var express = require('express')
     , app = module.exports = express();
+var http = require('http'),
+		server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+var fs = require('fs');
  
 // Using the .html extension instead of
 // having to name the views as *.ejs
@@ -20,21 +25,71 @@ var messages = [
   { name: 'Dr. Rockso', message: 'Where is my friend Toki?' }
 ];
 
+var evidenceMediaList = []; 
+var writeableMediaList = []; 
+
  
 // Serve the index page
 app.get('/', function(req, res){
   res.render('index', {
     pageTitle: 'EJS Demo',
-    messages: messages
+    evidenceMediaList: evidenceMediaList
   });
 });
 
 app.use(express.static("../E4A") );
  
 if (!module.parent) {
-  app.listen(8888);
-  console.log('EJS Demo server started on port 8080');
+  server.listen(8888);
+  console.log('EasyForensicAcquisition Server Started on port 8888');
 }
+
+
+/*
+var child = require('child_process').execFile('../scripts/monitorDriveLocations.pl', [ 
+     ], {
+		detached: true,
+		stdio: ['ignore', 1, 2 ]
+}); 
+
+child.unref();
+// use event hooks to provide a callback to execute when data are available: 
+child.stdout.on('data', function(data) {
+    console.log(data.toString()); 
+});
+
+child.stderr.on('data', function(data) {
+    console.log(data.toString()); 
+});
+*/
+
+
+// INTERVAL FOR CHECKING :
+// evidenceMedia & writeableMedia 
+// once per second for drives.
+var interval;
+interval = setInterval( function() {
+	var oldEMList = evidenceMediaList.slice();
+	evidenceMediaList = fs.readdirSync("/tmp");
+	
+	//NEED TO UPDATE LIST THAT IS SHOWN.
+	if(oldEMList != evidenceMediaList) {
+//		console.log("evidenceMediaList contents changed!");	
+	}
+//	for(var i in evidenceMediaList) {
+//		console.log(evidenceMediaList[i]);
+//	}
+	
+
+}, 1000);
+
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 
 
---
+++ scripts/monitorDriveLocations.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+
+my $evidenceMediaPath;
+my $writeableMediaPath;
+
+while( 1 ) {
+	print "Test1\n";
+
+
+	sleep(1);
+}
+
+
+exit(0);
+
